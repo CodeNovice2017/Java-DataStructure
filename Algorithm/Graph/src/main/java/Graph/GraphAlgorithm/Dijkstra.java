a8d5ce15a8ade7b626bc80ba222452407f265f71
CodeNovice <codenovice2017@gmail.com>
2021-01-11 20:15:08 +0800

    Dijkstra Algorithm Study2

--- Algorithm/Graph/src/main/java/Graph/GraphAlgorithm/Dijkstra.java
+++ Algorithm/Graph/src/main/java/Graph/GraphAlgorithm/Dijkstra.java
@@ -8,20 +8,23 @@ import Graph.Edge;
 import Graph.Node;
 
 public class Dijkstra {
-    
     public static HashMap<Node, Integer> dijkstra1(Node head){
         // 返回的结果Map,本身维护的是从head节点到该key的Node节点的最短距离
         HashMap<Node,Integer> distanceMap = new HashMap<>();
         // selectedSet不是判断是否已经加入distanceMap,而是代表该节点是否已经解锁
         // 比如最开始head节点是先加入distanceMap,传入head到head本身的距离0
-        // 此时并不将head节点加入selectedMap,因为此时head还没有解锁,
-        // 解锁的含义我理解是要遍历该节点所相邻的所有节点,因为此时起点到该节点的距离是当前时刻最短的
-        // 而我们要做的就是判断从起点到该点的距离加上该点和相邻点的距离,和之前维护在distanceMap中起点通过其他路径到和该点相邻节点距离中较小的值
-        // 所以此时相当于还没有判断(head到相邻的节点的距离加上head到head的距离)与(之前distanceMap中,head到该head相邻点的距离(之前distanceMap中没有数据呢,所以distance在getMinDistanceAndUnselectedNode函数中都是Integer.MAX_VALUE))
-        // 所以不能在此时将head放入selectedSet
+        // 此时并不将head节点加入selectedMap,因为此时head还没有进行松弛操作,
+        // 松弛操作的含义我理解是要遍历该节点所相邻的所有节点,因为此时起点到该节点的距离是最短的(通过getMinDistanceAndUnselectedNode找出的节点就是起点到该节点最短路径的节点,不可能之后解锁的节点反而更近)
+        // 而松弛操作要做的工作是计算从起点到该点的距离加上该点和相邻点的距离,和之前维护在distanceMap中起点通过其他路径到和该点相邻节点距离中较小的值,因为本身distanceMap.get(minNode)已经是起点到达minNode的最短路径了,即使之后有到minNode相邻接点的更短的路径,那这条路径也不可能经过minNode中转
+        // 只有上面说的松弛操作的工作做完了,起点到minNode的最短距离也确定了,才将minNode加入selectedSet
+        // 而对于此时的情景来说就是minNode就是head,按照上面说的流程,此时通过getMinDistanceAndUnselectedNode找出的就是还没有进行松弛操作的节点中到达head起点最短路径的节点(最开始就是距离为0的head)
+        // 然后将通过minNode中转的所有相邻节点的distance加入distanceMap中,因为之后即使有起点到这些相邻节点更短的路径,也绝不会经过minNode了,因为起点到minNode已经是确认的最短路径了
+
+        // 所以加入到selectedSet就代表着该节点的最短路径已经算出,通过该节点可以到达的相邻接点的路径距离已经更新在distanceMap中了,以后再也不用管我了
+
+        // 也就是松弛操作之后解锁这个节点
         HashSet<Node> selectedSet = new HashSet<>();
         distanceMap.put(head, 0);
-        // 就相当于更新以这个被找出的最短路径节点为中转的相邻节点到起点的距离和原本相邻节点之前到起点距离的更小值之后,才相当于把这个节点解锁了
         Node minNode = getMinDistanceAndUnselectedNode(distanceMap, selectedSet);
         while(minNode != null){
             for (Edge tempEdge : minNode.edges) {
@@ -38,7 +41,6 @@ public class Dijkstra {
         return distanceMap;
     }
 
-    // 找出当前distanceMap中起点到其他节点(并且该节点之前并未作为中转点解锁过)中的最短路径的那个终点节点
     public static Node getMinDistanceAndUnselectedNode(HashMap<Node, Integer> distanceMap, HashSet<Node> touchedNodes) {
         Node minNode = null;
         int minDistance = Integer.MAX_VALUE;
