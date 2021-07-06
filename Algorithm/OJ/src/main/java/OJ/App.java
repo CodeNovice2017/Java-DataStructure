3145e7dedb970e96a57b1b6d45902fc53fdebf02
CodeNovice <codenovice2017@gmail.com>
2021-07-06 10:02:42 +0800

    冗余代码清理

--- Algorithm/OJ/src/main/java/OJ/App.java
+++ Algorithm/OJ/src/main/java/OJ/App.java
@@ -2,10 +2,6 @@ package OJ;
 
 import OJ.problems.Shopee20210705;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-
 public class App {
 
     public static void main(String[] args) {
@@ -17,22 +13,5 @@ public class App {
         int[] endPoint = new int[]{2, 2};
         Shopee20210705 shopee20210705 = new Shopee20210705();
         System.out.println(shopee20210705.minimumInitHealth(rooms, startPoint, endPoint));
-    }
-
-    public void A() {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        HashSet<String> visited = new HashSet<>();
-        int[][] gra = new int[200][200];
-        PriorityQueue<String> priorityQueue = new PriorityQueue<String>((a, b) -> {
-            return hashMap.get(a) - hashMap.get(b);
-        });
-        hashMap.put(0 + "@" + 0, -2);
-        priorityQueue.add(0 + "@" + 0);
-        while (priorityQueue.size() > 0) {
-            String[] tem = priorityQueue.poll().split("@");
-            int x = Integer.valueOf(tem[0]);
-            int y = Integer.valueOf(tem[1]);
-            int val  = hashMap.get()
-        }
     }
 }
